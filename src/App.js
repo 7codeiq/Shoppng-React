2fe4563b1e5a038ac3199fb389085f77790c80f2
7codeiq <mortada.teach@outlook.com>
2022-12-05 13:53:09 +0300

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import React from 'react'
-import {BrowserRouter as Router, Route , Routes} from 'react-router-dom'
+import { Routes, Route} from "react-router-dom";
 import {Home, Products, Shopping ,About ,Contact} from '../src/pages/index'
 
 const App = () => {
   return (
     <>
-       <Router>
           <Routes>
 
              <Route path='/' element={<Home/>} />
@@ -15,7 +14,6 @@ const App = () => {
              <Route path='/About' element={<About/>} />
              <Route path='/Contact' element={<Contact/>} />
           </Routes>
-       </Router>
     </>
 
   );
